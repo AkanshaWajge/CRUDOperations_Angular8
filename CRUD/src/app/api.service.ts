415da0bbe04f27e14072e40659a5db4503be11ca
Akansha Wajge <akanshad.wajge@gmail.com>
2019-11-05 15:32:03 +0530

    Create & Update Operations in Angular

---
+++ CRUD/src/app/api.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Todo } from './todo';
+
+
+
+// const apiUrl = "http://localhost:3000/";
+ 
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(public http :HttpClient) { }
+  getTodos (): Observable<Todo[]> {
+    return this.http.get<Todo[]>("http://localhost:3000/posts", httpOptions)
+      .pipe(
+        tap(heroes => console.log('fetched todos')),
+        catchError(this.handleError('getTodos', []))
+      );
+  }
+   
+  getTodo(id: number): Observable<Todo> {
+    const apiUrl = "http://localhost:3000/posts";
+    const url = `${apiUrl}?id=${id}`;
+    return this.http.get<Todo>(url).pipe(
+      tap(_ => console.log(`fetched todo id=${id}`)),
+      catchError(this.handleError<Todo>(`getTodo id=${id}`))
+    );
+  }
+   
+  addTodo (todo): Observable<Todo> {
+    const apiUrl = "http://localhost:3000/posts/";
+    return this.http.post<Todo>(`${apiUrl}`, todo, httpOptions).pipe(
+      tap((todo: Todo) => console.log(`added todo w/ id=${todo.id}`)),
+      catchError(this.handleError<Todo>('addTodo'))
+    );
+  }
+   
+  updateTodo (id, todo): Observable<any> {
+    const apiUrl= "http://localhost:3000/posts/";
+    const url = `${apiUrl}/${1}`;
+    return this.http.put(url, todo, httpOptions).pipe(
+      tap(_ => console.log(`updated todo id=${id}`)),
+      catchError(this.handleError<any>('updateTodo'))
+    );
+  }
+   
+  // deleteTodo (id): Observable<Todo> {
+  //   const apiUrl= "http://localhost:3000/posts/";
+  //   const url = `${apiUrl}/${id}`;
+   
+  //   return this.http.delete<Todo>(url, httpOptions).pipe(
+  //     tap(_ => console.log(`deleted todo id=${id}`)),
+  //     catchError(this.handleError<Todo>('deletetodo'))
+  //   );
+  // }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); 
+      return of(result as T);
+    };
+  }
+}
---
+++ CRUD/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoAddComponent,
+    TodoEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
